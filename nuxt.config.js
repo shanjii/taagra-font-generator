b91a4467698618db401a313c086eda0d385de148
Victor Aro <victor.galves@live.com>
2021-12-08 01:56:36 -0300

    removed github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,6 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  target: 'static',
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'taagra-font-generator',
