14cb500854f873c0029fd2c1369515a5b7f77f8f
Victor Aro <victor.galves@live.com>
2021-12-08 02:17:19 -0300

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,6 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
-  target: 'static',
-   router: {
-      base: '/<repository-name>/'
-   },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'taagra-font-generator',
